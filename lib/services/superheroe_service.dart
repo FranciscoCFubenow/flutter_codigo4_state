55747cecc3fa0577c1c124df0688e16a10426097
Francisco Caceres Camilo <francisco.caceres@fubenow.com>
2022-02-25 20:24:59 -0300

    Service creado con create y close

--- lib/services/superheroe_service.dart
+++ lib/services/superheroe_service.dart
@@ -6,4 +6,13 @@ class SuperheroeService {
   Superheroe? _superheroe;
 
   StreamController<Superheroe> _superheroeStream = StreamController();
+
+  createSuperhero(Superheroe superheroe) {
+    _superheroe = superheroe;
+    _superheroeStream.add(_superheroe!);
+  }
+
+  closeStream() {
+    _superheroeStream.close();
+  }
 }
